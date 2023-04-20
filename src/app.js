a1960206151fe321ef012815ed5dfe297cdff389
Utkarsh Raj Sinha <utkarshcseit@gmail.com>
2023-04-20 05:52:16 +0530

    code

--- src/app.js
+++ src/app.js
@@ -11,13 +11,13 @@ app.use(express.json());
 // Write GET endpoint for sending all the products to client here
 // Endpoint - /api/v1/products
 app.get("api/v1/products",(req,res)=>{
- res.status(200).json({
-    status:"success",
-    message:"Product fetched successfully",
-    data:{
-      products,
-    },
- });
+    res.status(200).json({
+         status:"success",
+         message:"Product fetched successfully",
+         data:{
+         products,
+         },
+    });
 });
 
 module.exports = app;
